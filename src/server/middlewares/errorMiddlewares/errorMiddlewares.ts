e0077b8ec99f23ed34aba960de0e30b7a6eecd55
Cristina <cristinajmartin98@gmail.com>
2023-02-24 20:19:44 +0100

    Add connectDatabase function

--- src/server/middlewares/errorMiddlewares/errorMiddlewares.ts
+++ src/server/middlewares/errorMiddlewares/errorMiddlewares.ts
@@ -2,7 +2,7 @@ import { type NextFunction, type Request, type Response } from "express";
 import createDebug from "debug";
 import { CustomError } from "../../../customError/customError.js";
 
-export const debug = createDebug("feisbuk:errors");
+export const debug = createDebug("feisbuk:server:middlewares:errorMiddleware");
 
 export const notFoundError = (
   req: Request,
