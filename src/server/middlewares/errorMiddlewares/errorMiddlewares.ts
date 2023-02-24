1eb3eb23020c32515fb3bd120ece3806fd2eca9a
Cristina <cristinajmartin98@gmail.com>
2023-02-24 19:25:27 +0100

    Add generalError middleware

---
+++ src/server/middlewares/errorMiddlewares/errorMiddlewares.ts
@@ -0,0 +1,18 @@
+import { type NextFunction, type Request, type Response } from "express";
+import createDebug from "debug";
+import { type CustomError } from "../../../customError/customError.js";
+
+export const debug = createDebug("feisbuk:errors");
+
+export const generalError = (
+  error: CustomError,
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  debug(error.message);
+
+  res
+    .status(error.statusCode || 500)
+    .json({ error: error.publicMessage || "Something went wrong :(" });
+};
