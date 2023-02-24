5366be2a2e3bab2d16d987dcbb97097ae6efc832
Cristina <cristinajmartin98@gmail.com>
2023-02-24 19:46:15 +0100

    Add notFoundError middleware

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,9 +1,13 @@
 import express from "express";
 import pong from "./controllers/pong/pong.js";
-import { generalError } from "./middlewares/errorMiddlewares/errorMiddlewares.js";
+import {
+  generalError,
+  notFoundError,
+} from "./middlewares/errorMiddlewares/errorMiddlewares.js";
 
 export const app = express();
 
 app.get("/", pong);
 
+app.use("/", notFoundError);
 app.use("/", generalError);
--- src/server/middlewares/errorMiddlewares/errorMiddlewares.ts
+++ src/server/middlewares/errorMiddlewares/errorMiddlewares.ts
@@ -1,8 +1,22 @@
 import { type NextFunction, type Request, type Response } from "express";
 import createDebug from "debug";
-import { type CustomError } from "../../../customError/customError.js";
+import { CustomError } from "../../../customError/customError.js";
 
 export const debug = createDebug("feisbuk:errors");
+
+export const notFoundError = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const notFoundError = new CustomError(
+    "Wrong endpoint",
+    404,
+    "Endpoint not found"
+  );
+
+  next(notFoundError);
+};
 
 export const generalError = (
   error: CustomError,
