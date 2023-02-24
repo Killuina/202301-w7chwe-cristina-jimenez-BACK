b212b3d8379873f6b220a1f5fa1be36a254338f0
Cristina <cristinajmartin98@gmail.com>
2023-02-24 19:26:11 +0100

    Register generalError middleware

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,6 +1,9 @@
 import express from "express";
 import pong from "./controllers/pong/pong.js";
+import { generalError } from "./middlewares/errorMiddlewares/errorMiddlewares.js";
 
 export const app = express();
 
 app.get("/", pong);
+
+app.use("/", generalError);
