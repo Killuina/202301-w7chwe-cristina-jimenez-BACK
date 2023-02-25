dd0e45b93913641a6c20edf7466960980fdf955d
Cristina <cristinajmartin98@gmail.com>
2023-02-25 19:31:31 +0100

    Add login endpoint

--- src/server/routers/usersRouter.ts
+++ src/server/routers/usersRouter.ts
@@ -1,11 +1,16 @@
 import { Router } from "express";
-import { registerUser } from "../controllers/usersControllers/usersControllers.js";
+import {
+  loginUser,
+  registerUser,
+} from "../controllers/usersControllers/usersControllers.js";
 import multerMiddleware from "../middlewares/multerMiddleware.js";
 
 const registerEndpoint = "/register";
+const loginEndpoit = "/login";
 
 const usersRouter = Router();
 
 usersRouter.post(registerEndpoint, multerMiddleware, registerUser);
+usersRouter.post(loginEndpoit, loginUser);
 
 export default usersRouter;
